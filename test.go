82867a8e9832d0f3455ce7b1be07c6faa1897441
lixiaolong <li_span@sina.com>
2020-02-01 15:59:04 +0800

    新增椭圆曲线加密算法

--- test.go
+++ test.go
@@ -1,21 +1,10 @@
 package main
-//
-//import "fmt"
-//
-//func adder() func(int) int {
-//	sum := 0
-//	return func(x int) int {
-//		sum += x
-//		return sum
-//	}
-//}
-//
-//func main() {
-//	pos, neg := adder(), adder()
-//	for i := 0; i < 10; i++ {
-//		fmt.Println(
-//			pos(i),
-//			neg(-2*i),
-//		)
-//	}
-//}
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(len(a))
+	fmt.Println(a[0 : len(a)/2])
+	fmt.Println(a[len(a)/2:])
+}
